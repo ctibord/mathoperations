af3e08df27507848aa22ec26de2371a1ff582389
jarkakr <91731780+jarkakr@users.noreply.github.com>
2021-10-02 16:02:26 +0200

    Digit sum, factorial

--- mathoperations.c
+++ mathoperations.c
@@ -8,6 +8,8 @@ int subtract(int count, ...);
 int multiply(int count, ...);
 int intdiv(int num1, int num2, int modulo);
 float realdiv(int num1, int num2);
+int digitsum(int num);
+long factorial(int num);
 
 int sum(int count, ...) {
     int result = 0, i;
@@ -58,4 +60,21 @@ float realdiv(int num1, int num2) {
     if (num2 == 0)
         return (float) 0;
     else return (float) num1 / (float) num2;
+}
+
+int digitsum(int num) {
+    int sum = 0;
+    while (num != 0) {
+        sum += num % 10;
+        num /= 10;
+    }
+    return sum;
+}
+
+long factorial(int num) {
+    long res = 1;
+    int i;
+    for (i = 2; i <= num; i++)
+        res *= (long) i;
+    return res;
 }

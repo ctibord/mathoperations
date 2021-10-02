d197c54360c1dc004959763ec2eb5d80f9d79e7b
vercakr <91791254+vercakr@users.noreply.github.com>
2021-10-02 16:59:48 +0200

    Digit count, Fibonacci sequence in tests

--- tests/main.c
+++ tests/main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "mathoperations.h"
 
 int main() {
     // MathOperations Tests
+    int *fib, i;
     printf("Sum of 1, 2, 3: %d\n", sum(3, 1, 2, 3));
     printf("Subtraction of 3, 1: %d\n", subtract(2, 3, 1));
     printf("Multiplication of 1, 2, 3, 4: %d\n", multiply(4, 1, 2, 3, 4));
@@ -11,5 +13,15 @@ int main() {
     printf("Real division of 3, 2: %f\n", realdiv(3, 2));
     printf("Digit sum of 426: %d\n", digitsum(426));
     printf("Factorial of 4: %ld\n", factorial(4));
+    printf("Digit count of 125: %d\n", digitcount(125));
+    printf("Getting Fibonacci sequence...\n");
+    fib = fibonacci(15);
+    if (fib != NULL) {
+        for (i = 0; i < 15; i++)
+            printf("%d ", fib[i]);
+        printf("\n\n");
+        free(fib);
+        fib = NULL;
+    } else printf("Error while getting Fibonacci sequence...\n");
     return 0;
 }
